e1fdf09ea5cff6577c4718417b9adf4e7128c7c2
thomasogorman <thomasogorman@gmail.com>
2016-04-11 19:56:15 +0100

    Initial commit

---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+'''
+a test project
+'''
+
+plt.figure()
+plt.plot(np.sin(np.arange(0,10,0.01)))
+plt.plot(np.cos(np.arange(0,10,0.01)))
+plt.show()
